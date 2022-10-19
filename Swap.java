dcb36175375d33bc1e239930279ff146529a1ac0
setha <sethan15@hotmail.com>
2022-10-19 23:12:38 +0530

    Swap

---
+++ Swap.java
@@ -0,0 +1,21 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a=10 ,b=20;
+
+//        int c=a;
+//        a=b;
+//        b=c;
+//        System.out.println("Swap the number " + a +" "+b);
+
+//        a=a+b;
+//        b=a-b;
+//        a=a-b;
+//        System.out.println("Swap the number " + a +" "+b);
+
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println("Swap the number " + a +" "+b);
+
+    }
+}
