401247a8bb8ab304ad4113f40a831f5da04a8843
setha <sethan15@hotmail.com>
2022-10-20 16:43:39 +0530

    Basic

---
+++ Interview/Count_the_num_odd_or_Even.java
@@ -0,0 +1,21 @@
+package Interview;
+
+public class Count_the_num_odd_or_Even {
+    public static void main(String[] args) {
+        int n = 1234567566;
+        int even_count = 0;
+        int odd_count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem % 2 == 0)
+                even_count++;
+            else
+                odd_count++;
+            n = n / 10;
+        }
+        System.out.println("Even count : " +
+                even_count);
+        System.out.println("Odd count : " +
+                odd_count);
+    }
+}
