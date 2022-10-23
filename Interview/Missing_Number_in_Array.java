9244d885f912471ad8760da2445d8c4859327be4
setha <sethan15@hotmail.com>
2022-10-23 23:22:52 +0530

    Basic

---
+++ Interview/Missing_Number_in_Array.java
@@ -0,0 +1,22 @@
+package Interview;
+
+public class Missing_Number_in_Array
+{
+    public static void main(String[] args) {
+        int arr1[] ={1,2,3,4};
+        int arr2[]={1,2,3,4,5};
+        int a = 0;
+
+        for(int i=0;i<arr1.length;i++)
+        {
+            for(int j=0;j<arr2.length;j++)
+            {
+              a=arr1[i]-arr2[i];
+
+            }
+
+        }
+        System.out.println(a);
+
+    }
+}
---
+++ Interview/checkTwoArrayAreEqual.java
@@ -0,0 +1,17 @@
+package Interview;
+
+public class checkTwoArrayAreEqual {
+    public static void main(String[] args) {
+        int arr1[] ={1,2,3,4};
+        int arr2[]={1,2,3,4,5};
+
+        if(arr1.equals(arr2))
+        {
+            System.out.println("Enterted number is Equal");
+        }else
+        {
+            System.out.println("Enterted number is Not Equal");
+        }
+    }
+
+}
