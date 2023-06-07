dff5bf092986d8f539154c24075c4d317733860a
setha <sethan15@hotmail.com>
2023-06-07 15:51:08 +0530

    String Practice

---
+++ Interview/SplitString.java
@@ -0,0 +1,14 @@
+package Interview;
+
+public class SplitString {
+    public static void main(String[] args) {
+        String s= "Scalerby Interview Bit";
+        String[] s1=  s.split(" ");
+
+
+        for (String word:s1) {
+            System.out.println(word);
+
+        }
+    }
+}
---
+++ Interview/String_Practise.java
@@ -0,0 +1,17 @@
+package Interview;
+
+public class String_Practise {
+
+	public static void main(String[] args) {
+		String s="chethan";
+		System.out.println(s.toUpperCase());
+		System.out.println(s);
+
+		StringBuffer sb=new StringBuffer(s);
+		sb.append("cjeyhs");
+		System.out.println(sb);
+		System.out.println(sb);
+
+	}
+
+}
