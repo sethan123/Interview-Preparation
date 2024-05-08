9de43aa5d13805c84edddd13e73548dcd0dc28fc
setha <sethan15@hotmail.com>
2024-05-08 16:28:31 +0530

    dd

---
+++ Java_Numbers/Indianrupee_to_Pound.java
@@ -0,0 +1,15 @@
+package Java_Numbers;
+
+public class Indianrupee_to_Pound {
+
+	public static void main(String[] args) {
+	
+		
+		int pound =30;
+		int rupee=2;
+		int calulate=pound*rupee;
+		System.out.println(calulate);
+
+	}
+
+}
---
+++ Java_Numbers/Test.java
@@ -0,0 +1,27 @@
+package Java_Numbers;
+
+import java.util.HashMap;
+
+//I need to crate sethhanG
+// Frequency of the string 
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s ="sethhan";
+		
+		String lower=s.toLowerCase();
+		String output;
+		
+		
+		
+		 
+		HashMap<String, Integer> hs = new HashMap<>();
+		System.out.println(hs.getOrDefault(lower, s.length()));
+		
+		
+		
+		
+	}
+
+}
